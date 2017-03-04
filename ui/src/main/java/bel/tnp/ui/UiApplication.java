cba8d81cce5db87f7e29602dc6fbe469254c1a6f
dmitrykyz <nike27mouse@gmail.com>
2017-03-04 15:07:18 +0300

    Add form login.html to input login and pass with auth from github and facebook

--- ui/src/main/java/bel/tnp/ui/UiApplication.java
+++ ui/src/main/java/bel/tnp/ui/UiApplication.java
@@ -2,12 +2,10 @@ package bel.tnp.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * Created by Dmitry on 2/28/2017.
